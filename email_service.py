d8489beca8523183bd1414c0dfbf05cf6bc459d3
moein <moein.kameli.mk@gmail.com>
2024-11-27 14:53:07 -0800

    UP\changed the code to support app_password instead of oauth so it have lifelong access to gmail

--- email_service.py
+++ email_service.py
@@ -1,137 +1,30 @@
-import os
-import base64
 import logging
-from abc import ABC, abstractmethod
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
-import boto3
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+import smtplib
 
 from config import Config
-
 
 logger = logging.getLogger(__name__)
 
 
-class BaseTokenHandler(ABC):
-    CREDENTIALS_PATH = None
-    TOKEN_PATH = None
-
-    @abstractmethod
-    def load_credentials(self):
-        pass
-
-    @abstractmethod
-    def save_token(self, creds):
-        pass
-
-
-class LocalTokenHandler(BaseTokenHandler):
-    CREDENTIALS_PATH = 'credentials.json'
-    TOKEN_PATH = 'token.json'
-
-    def load_credentials(self):
-        if os.path.exists(self.TOKEN_PATH):
-            creds = Credentials.from_authorized_user_file(self.TOKEN_PATH, ['https://www.googleapis.com/auth/gmail.send'])
-            return creds
-        return None
-
-    def save_token(self, creds):
-        with open(self.TOKEN_PATH, 'w') as token_file:
-            token_file.write(creds.to_json())
-
-
-class LambdaTokenHandler(BaseTokenHandler):
-    CREDENTIALS_PATH = '/tmp/credentials.json'
-    TOKEN_PATH = '/tmp/token.json'
-
-    def __init__(self):
-        super().__init__()
-        self.s3 = boto3.client('s3')
-        self.bucket_name = Config.get_env_variable('TOKEN_BUCKET')
-        self.credentials_key = Config.get_env_variable('CREDENTIALS_KEY', 'credentials.json')
-        self.token_key = Config.get_env_variable('TOKEN_KEY', 'token.json')
-
-    def load_credentials(self):
-        # Download credentials.json from S3
-        if not self.download_from_s3(self.credentials_key, self.CREDENTIALS_PATH):
-            logger.error("Unable to download credentials.json from S3. Cannot proceed.")
-            return None
-
-        # Load token if available
-        if self.download_from_s3(self.token_key, self.TOKEN_PATH):
-            creds = Credentials.from_authorized_user_file(self.TOKEN_PATH, ['https://www.googleapis.com/auth/gmail.send'])
-            return creds
-        return None
-
-    def save_token(self, creds):
-        with open(self.TOKEN_PATH, 'w') as token_file:
-            token_file.write(creds.to_json())
-        self.upload_to_s3(self.TOKEN_PATH, self.token_key)
-
-    def download_from_s3(self, s3_key, local_path):
-        try:
-            self.s3.download_file(self.bucket_name, s3_key, local_path)
-            return True
-        except Exception as e:
-            logger.error(f"Error downloading {s3_key} from S3: {e}")
-            return False
-
-    def upload_to_s3(self, local_path, s3_key):
-        try:
-            self.s3.upload_file(local_path, self.bucket_name, s3_key)
-        except Exception as e:
-            logger.error(f"Error uploading {local_path} to S3 as {s3_key}: {e}")
-
-
-def get_token_handler():
-    env = Config.get_env_variable('ENVIRONMENT', 'lambda')
-    if env == 'local':
-        logger.info("Initializing LocalTokenHandler.")
-        return LocalTokenHandler()
-    logger.info("Initializing LambdaTokenHandler.")
-    return LambdaTokenHandler()
-
-
 class SendEmail:
-    SCOPES = [Config.get_env_variable('GMAIL_SCOPES')]
     SENDER_EMAIL = Config.get_env_variable('SENDER_EMAIL')
     TO_EMAIL = Config.get_env_variable('TO_EMAIL')
+    APP_PASSWORD = Config.get_env_variable('APP_PASSWORD')
     SUBJECT = 'ICBC Road Test Notification'
 
-    def __init__(self):
-        self.token_handler = get_token_handler()
-        self.service = None
-
-    def initialize_service(self):
-        """Initialize the Gmail service."""
-        creds = self.token_handler.load_credentials()
-
-        # Refresh token if expired or run OAuth flow if no valid token exists
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-            logger.info("Token refreshed successfully.")
-        elif not creds:
-            # Run OAuth flow for local environment only
-            if isinstance(self.token_handler, LocalTokenHandler):
-                flow = InstalledAppFlow.from_client_secrets_file(self.token_handler.CREDENTIALS_PATH, self.SCOPES)
-                creds = flow.run_local_server(port=0)
-                logger.info("New token obtained successfully.")
-            else:
-                logger.error("No valid credentials found. Ensure the refresh token is initialized manually.")
-                raise ValueError("Unable to obtain valid credentials. Initialization failed.")
-
-        # Save the updated credentials
-        self.token_handler.save_token(creds)
-
-        self.service = build('gmail', 'v1', credentials=creds)
+    SMTP_SERVER = 'smtp.gmail.com'
+    SMTP_PORT = 587
 
     def create_message(self, subject, message_text):
         """Create an email message."""
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = f'ICBC Road Test Notifier{self.SENDER_EMAIL}'
+        msg['To'] = self.TO_EMAIL
+
+        # Create the body of the message (HTML version).
         html_content = "<html><body><h2>ICBC Road Test Availability</h2><ul>"
         logger.info("Message content prepared for sending.")
         for location, dates in message_text.items():
@@ -141,22 +34,20 @@ class SendEmail:
             html_content += "</ul></li>"
         html_content += "</ul></body></html>"
 
-        message = MIMEText(html_content, 'html')
-        message['to'] = self.TO_EMAIL
-        message['from'] = self.SENDER_EMAIL
-        message['subject'] = subject
-        raw_message = base64.urlsafe_b64encode(message.as_string().encode("utf-8"))
-        return {
-            'raw': raw_message.decode("utf-8")
-        }
+        # Attach HTML content
+        part = MIMEText(html_content, 'html')
+        msg.attach(part)
+        return msg
 
     def send_message(self, message):
-        """Send the email message."""
+        """Send the email message using Gmail SMTP."""
         try:
-            sent_message = self.service.users().messages().send(
-                userId='me', body=message
-            ).execute()
-            logger.info(f'Message sent. ID: {sent_message["id"]}')
-        except HttpError as e:
-            logger.error(f'Failed to send message: {e}')
+            server = smtplib.SMTP(self.SMTP_SERVER, self.SMTP_PORT)
+            server.starttls()  # Upgrade the connection to a secure encrypted SSL/TLS connection
+            server.login(self.SENDER_EMAIL, self.APP_PASSWORD)
+            server.sendmail(self.SENDER_EMAIL, self.TO_EMAIL, message.as_string())
+            server.quit()
+            logger.info("Email sent successfully!")
+        except Exception as e:
+            logger.error(f"Failed to send message: {e}")
             raise e
--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def main():
     # Step 2: Send an email if there are available dates
     if availability_finder.do_email:
         gmail_service = SendEmail()
-        gmail_service.initialize_service()
         message = gmail_service.create_message(
             subject="ICBC road test available dates",
             message_text=availability_finder.available_dates_result
